b13d89ce8280bae647ee9fb331edb49a7b60e118
Anastasia Lyah <lyahanastasya@gmail.com>
2024-10-30 13:32:23 +0200

    com1

--- AADS_LR3_Task1/Program.cs
+++ AADS_LR3_Task1/Program.cs
@@ -13,34 +13,11 @@ namespace AADS_LR3_Task1
             Queue<int> queue = new Queue<int>();
             foreach (int num in numbers)
             {
-                queue.Enqueue(num); 
+                queue.Enqueue(num);
             }
             return queue;
         }
-        static void ClearQueue(Queue<int> queue)
-        {
-            queue.Clear();
-            Console.WriteLine("Чергу очищено.");
-        }
-        static void EnqueueElement(Queue<int> queue, int element)
-        {
-            queue.Enqueue(element);
-            Console.WriteLine($"Елемент {element} додано до черги.");
-        }
-        static int DequeueElement(Queue<int> queue)
-        {
-            if (queue.Count > 0)
-            {
-                int removedElement = queue.Dequeue();
-                Console.WriteLine($"Елемент {removedElement} видалено з черги.");
-                return removedElement;
-            }
-            else
-            {
-                Console.WriteLine("Черга порожня.");
-                return -1;
-            }
-        }
+
         static void SwapFirstAndLast(Queue<int> queue)
         {
             if (queue.Count > 1)
@@ -49,6 +26,7 @@ namespace AADS_LR3_Task1
                 int temp = queueArray[0];
                 queueArray[0] = queueArray[queueArray.Length - 1];
                 queueArray[queueArray.Length - 1] = temp;
+
                 queue.Clear();
                 foreach (int num in queueArray)
                 {
@@ -61,9 +39,14 @@ namespace AADS_LR3_Task1
                 Console.WriteLine("Має бути принаймнi два елементи для обмiну.");
             }
         }
+
         static void Main(string[] args)
         {
-            int[] numbers = { 11, 22, 33, 44, 55 };
+            Console.WriteLine("Введiть числа через пробiл для створення черги:");
+            string input = Console.ReadLine();
+
+            int[] numbers = Array.ConvertAll(input.Split(), int.Parse);
+
             Queue<int> queue = CreateQueue(numbers);
 
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
@@ -85,21 +68,6 @@ namespace AADS_LR3_Task1
                 Console.Write(num + " ");
             }
             Console.WriteLine();
-
-            DequeueElement(queue);
-
-            EnqueueElement(queue, 66);
-
-            Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            Console.WriteLine("Оновлена черга:");
-            Console.ResetColor();
-            foreach (int num in queue)
-            {
-                Console.Write(num + " ");
-            }
-            Console.WriteLine();
-
-            ClearQueue(queue);
         }
     }
 }
